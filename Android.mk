c46e38af93cbd46dc9fad951276890423da2aaab
Yukino1314520 <1475215798@qq.com>
2025-02-18 16:50:28 +0800

    Add Redmi Note12T Pro code name

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter aristotle plato rubens xaga,$(TARGET_DEVICE)),)
+ifneq ($(filter aristotle plato rubens xaga,pearl,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
